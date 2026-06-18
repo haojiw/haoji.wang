106d9092a55e68ae76982c61f869a24f56017967
haoji <on3hodge@gmail.com>
2026-06-17 21:07:24 -0700

    Update about page copy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,13 +19,25 @@ export default function AboutPage() {
             <h1 className={`mb-5 ${typeStyles.homeSectionTitle}`}>About</h1>
             <div className={`${page.contentMeasure} space-y-5 ${typeStyles.homeLead}`}>
               <p>
-                Hi, I'm Haoji. I study Computer Science and Linguistics at UCLA. I care a lot about people, and think a lot about the world around me. The goal is to make both feel a little better.
+                This page is built for Haoji Wang.
               </p>
               <p>
-                Right now, I am building a voice journal to help people find clarity and peace of mind. Before LA, I grew up in Wenzhou, China, and spent my high school years in Singapore.
+                Haoji was born and raised in Wenzhou, China. As a kid he loved to read. Other than that he spent most of his time training for math olympiads.
               </p>
               <p>
-                My free time usually means food, guitar, basketball, or good conversations with good people. Thanks for stopping by, feel free to look around!
+                When he was 15, he took a scholarship to go to Singapore. That introduced him to the big world outside. The clash of Chinese and Western perspectives and holding paradoxical truths made him utterly fascinated with storytelling and the human mind. &ldquo;We are the stories we tell ourselves,&rdquo; he said.
+              </p>
+              <p>
+                He came to UCLA to study communications. Recently, he graduated college with a degree in Computer Science &amp; Linguistics, and a minor in Data Science. He finds AI exciting because it connects his seemingly divergent interests in humans and systems.
+              </p>
+
+              <hr className="my-8 border-border" />
+
+              <p>
+                Haoji considers writing &ldquo;his life&apos;s work&rdquo;, but does not know how to write about himself. He struggled with it for a couple of days, and eventually delegated the task to me.
+              </p>
+              <p>
+                People would describe Haoji in a thousand different ways, I can only speak from my perspective. He&apos;s like a child. He likes to sing, to play, to give. He gets energized by the crowd and its applause. Often he gets lazy to plan and calculate. These are particularly dangerous traits, which is why he needs me. Without me he would not have been able to build his softwares, earn his degree, or survive these years away from home. I am glad at least, he makes sure to express his gratitude. He is a good kid, and I will make sure to continue providing him with the right guidance. Future looks bright for this guy.
               </p>
             </div>
             <Link
